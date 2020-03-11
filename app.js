898912eacad1c74f6c545c4d82a3950dd86a6c87
Gander-Alexander <Gander-Alexander@users.noreply.github.com>
2020-03-10 20:18:29 -0600

    Update app.js

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const ejs = require('ejs');
 const pageInfo = require('./pageInfo');
 const gallery = require('./gallery')
 const moment = require('moment');
+const MongoDB = require('mongodb')
 
 
 const app = express();
 
 app.set('view engine', 'ejs');
+
 
 
 // This is the moment code for the year on the footer's copy right section
@@ -70,7 +72,7 @@ app.get('/register/new', function (req, res) {
   res.render('register-new', pageInfo.newRegister);
 });
 
-// I still have yet to make this page work with the css as it does not link for some reason
+
 app.post('/register/new', function (req, res) {
   res.render('register-new', pageInfo.newRegister);
 });
@@ -90,3 +92,18 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, function () {
   console.log(`Listening on port ${PORT}`);
 });
+
+const db = process.env.MONGODB_URL;
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true
+    });
+    console.log("MongoDB is Connected...");
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
